8f135b21795c3c97d42bb9e3481c0f8d731b0ce0
zhangtianliang <zhangtianliang@g7.com.cn>
2021-05-08 17:05:23 +0800

    暂存

--- JS/12-promise.js
+++ JS/12-promise.js
@@ -240,6 +240,9 @@ MyPromise.race = function (promises) {
     })
 }
 
+const p = Promise.race([p1, p2, p3]);
+//只要p1、p2、p3之中有一个实例率先改变状态，p的状态就跟着改变。那个率先改变的 Promise 实例的返回值，就传递给p的回调函数。
+
 //all方法(获取所有的promise，都执行then，把结果放到数组，一起返回)
 MyPromise.all = function (promises) {
     let arr = [];
@@ -261,5 +264,15 @@ MyPromise.all = function (promises) {
         };
     });
 }
+
+const p = Promise.all([p1, p2, p3]);
+// Promise.all()方法接受一个数组作为参数，p1、p2、p3都是 Promise 实例，如果不是，就会先调用Promise.resolve方法，将参数转为 Promise 实例，
+// 再进一步处理。另外，Promise.all()方法的参数可以不是数组，但必须具有 Iterator 接口，且返回的每个成员都是 Promise 实例。
+
+// p的状态由p1、p2、p3决定，分成两种情况。
+
+// （1）只有p1、p2、p3的状态都变成fulfilled，p的状态才会变成fulfilled，此时p1、p2、p3的返回值组成一个数组，传递给p的回调函数。
+
+// （2）只要p1、p2、p3之中有一个被rejected，p的状态就变成rejected，此时第一个被reject的实例的返回值，会传递给p的回调函数。
 
 
