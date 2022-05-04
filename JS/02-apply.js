10ae5102e299128556f066323759469f1c5cbba4
baseRed <18539279350@163.com>
2022-05-04 09:01:10 +0800

    apply update

--- JS/02-apply.js
+++ JS/02-apply.js
@@ -2,11 +2,11 @@ Function.prototype.myapply = function(context){
     if(typeof this !== 'function'){
         throw new Error('error')
     }
-    // 还需考虑 context 为 数字 字符串的情况
-    context = context || window
+
+    context = !context ? window : typeof context === 'object' ? context : {},
     context.fn = this
-    const args = [...arguments].slice(1)
-    const result = context.fn(args)
+    const args = [...arguments][1]
+    const result = context.fn(...args)
     delete context.fn
     return result
 }
