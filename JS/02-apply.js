e34ee88fce7547c42dbf5e5ab2464fd4dd4b10a9
baseRed <18539279350@163.com>
2022-05-04 15:32:59 +0800

    apply update

--- JS/02-apply.js
+++ JS/02-apply.js
@@ -1,12 +1,39 @@
-Function.prototype.myapply = function(context){
-    if(typeof this !== 'function'){
-        throw new Error('error')
-    }
-
-    context = !context ? window : typeof context === 'object' ? context : {},
-    context.fn = this
-    const args = [...arguments][1]
-    const result = context.fn(...args)
-    delete context.fn
-    return result
+Function.prototype.myapply = function( context ){
+	if(typeof this !== 'function'){
+		throw new Error('is not function')
+	}
+	
+	context = !context ? window : typeof context === 'object' ? context : {}
+	context.fn = this
+	const args = [...arguments][1] ? [...arguments][1] : []
+	const result = context.fn(...args)
+	
+	delete context.fn
+	
+	return result
 }
+
+function say(name, age){
+	console.log(name,age)
+	return {
+		name,
+		age
+	}
+}
+
+// say.myapply(window,['zhangsan', 20])
+
+const o = {
+	name: 'zhangsan',
+	age: 20,
+	say: function(a,b){
+		console.log(this,this.name,this.age)
+		return {
+			a,
+			b
+		}
+	}
+	
+}
+
+console.log(o.say.myapply({name: 'lisi',age:40}, ['a', 'b']))
